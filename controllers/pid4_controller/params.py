992f822b8374b2b0e1e546b7d9ca859fd5f30bd1
2black0 <2black0@gmail.com>
2022-08-03 08:44:21 +0700

    update

---
+++ controllers/pid4_controller/params.py
@@ -0,0 +1,17 @@
+roll_param = [1, 0, 0]
+pitch_param = [1, 0, 0]
+yaw_param = [0.1, 0, 1]
+thro_param = [2, 0.2, 6]
+
+roll_input_param = [2, 0.1, 5]
+pitch_input_param = [2, 0.1, 5]
+vertical_thrust = 68.5
+
+x_target = 0.0
+y_target = 0.0
+z_target = 0.0
+yaw_target = 0.0
+
+status_takeoff = False
+status_landing = False
+status_gimbal = False
