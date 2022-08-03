e8cc1d4b52ef1a32cdecb653f365e7fcad488e64
2black0 <2black0@gmail.com>
2022-08-03 18:18:30 +0700

    update

--- controllers/pid4_controller/params.py
+++ controllers/pid4_controller/params.py
@@ -1,10 +1,10 @@
-x_param = [1, 0.5, 15]
-y_param = [1, 0.5, 15]
+x_param = [2, 0.0001, 5]
+y_param = [2, 0.0001, 5]
 z_param = [3, 0.1, 5]
 
-roll_param = [0.5, 0, 0.05]
-pitch_param = [0.5, 0, 0.05]
-yaw_param = [0.2, 0.01, 0.5]
+roll_param = [0.55, 0, 0.4]
+pitch_param = [0.65, 0, 0.5]
+yaw_param = [0.75, 0, 0.25]
 
 vertical_thrust = 68.5
 
@@ -12,6 +12,8 @@ x_target = 0.0
 y_target = 0.0
 z_target = 0.0
 yaw_target = 0.0
+roll_target = 0.0
+pitch_target = 0.0
 
 status_takeoff = False
 status_landing = False
--- controllers/pid4_controller/pid4_controller.py
+++ controllers/pid4_controller/pid4_controller.py
@@ -21,18 +21,10 @@ yPID = PID(float(y_param[0]), float(y_param[1]), float(y_param[2]), setpoint=flo
 zPID = PID(float(z_param[0]), float(z_param[1]), float(z_param[2]), setpoint=float(z_target))
 yawPID = PID(float(yaw_param[0]), float(yaw_param[1]), float(yaw_param[2]), setpoint=float(yaw_target))
 
-xPID.output_limits = (-10, 10)
-yPID.output_limits = (-10, 10)
+xPID.output_limits = (-2.5, 2.5)
+yPID.output_limits = (-2.5, 2.5)
 zPID.output_limits = (-5, 5)
-yawPID.output_limits = (-5, 5)
-
-
-def convert_to_attitude(x_error, y_error, yaw):
-    c, s = np.cos(yaw), np.sin(yaw)
-    R = np.array(((c, -s), (s, c)))
-    converted = np.matmul([x_error, y_error], R)
-    return converted
-
+yawPID.output_limits = (-2.5, 2.5)
 
 while robot.step(timestep) != -1:
     roll, pitch, yaw = sensor.read_imu()
@@ -50,28 +42,52 @@ while robot.step(timestep) != -1:
             sleep(0.25)
             break
         if key == Keyboard.UP:
-            z_target += 0.01
+            z_target += 0.1
             print("z_target=", z_target)
             break
         if key == Keyboard.DOWN:
-            z_target -= 0.01
+            z_target -= 0.1
             print("z_target=", z_target)
             break
         if key == ord("W"):
-            x_target -= 0.1
+            x_target -= 1
             print("x_target=", x_target)
             break
         if key == ord("S"):
-            x_target += 0.1
+            x_target += 1
             print("x_target=", x_target)
             break
         if key == ord("A"):
-            y_target += 0.1
+            y_target += 1
             print("y_target=", y_target)
             break
         if key == ord("D"):
-            y_target -= 0.1
+            y_target -= 1
             print("y_target=", y_target)
+            break
+        if key == Keyboard.RIGHT:
+            yaw_target += 1
+            if yaw_target >= 180:
+                yaw_target = 180
+            elif yaw_target <= -180:
+                yaw_target = -180
+            print("yaw_target=", yaw_target)
+            break
+        if key == Keyboard.LEFT:
+            yaw_target -= 1
+            if yaw_target >= 180:
+                yaw_target = 180
+            elif yaw_target <= -180:
+                yaw_target = -180
+            print("yaw_target=", yaw_target)
+            break
+        if key == Keyboard.HOME:
+            print(" Go Home")
+            x_target = 0.0
+            y_target = 0.0
+            z_target = 10.0
+            yaw_target = 0.0
+            sleep(0.25)
             break
 
     xPID.setpoint = x_target
@@ -79,27 +95,17 @@ while robot.step(timestep) != -1:
     zPID.setpoint = z_target
     yawPID.setpoint = yaw_target
 
-    # roll_error = yPID(ypos)
-    # pitch_error = xPID(xpos)
-
-    x_error = xpos - x_target
-    y_error = ypos - y_target
-
-    pitch_error, roll_error = convert_to_attitude(x_error, y_error, head)
+    roll_error = yPID(ypos)
+    pitch_error = xPID(xpos)
 
     vertical_input = zPID(zpos)
-    roll_input = (roll_param[0] * roll) + (roll_param[2] * roll_accel) + np.clip(roll_error, -3, 3)  #
-    pitch_input = (pitch_param[0] * pitch) - (pitch_param[2] * pitch_accel) - np.clip(pitch_error, -3, 3)  #
+    roll_input = (roll_param[0] * roll) + (roll_param[2] * roll_accel) - roll_error
+    pitch_input = (pitch_param[0] * pitch) - (pitch_param[2] * pitch_accel) + pitch_error
     yaw_input = yawPID(head)
 
-    motor_fl = np.clip((vertical_thrust + vertical_input - roll_input - pitch_input - yaw_input), 0, 500)
-    motor_fr = np.clip((vertical_thrust + vertical_input + roll_input - pitch_input + yaw_input), 0, 500)
-    motor_rl = np.clip((vertical_thrust + vertical_input - roll_input + pitch_input + yaw_input), 0, 500)
-    motor_rr = np.clip((vertical_thrust + vertical_input + roll_input + pitch_input - yaw_input), 0, 500)
+    motor_fl = np.clip((vertical_thrust + vertical_input - roll_input - pitch_input - yaw_input), 0, 100)
+    motor_fr = np.clip((vertical_thrust + vertical_input + roll_input - pitch_input + yaw_input), 0, 100)
+    motor_rl = np.clip((vertical_thrust + vertical_input - roll_input + pitch_input + yaw_input), 0, 100)
+    motor_rr = np.clip((vertical_thrust + vertical_input + roll_input + pitch_input - yaw_input), 0, 100)
 
-    print(
-        "pitch_error={: .2f}|pitch_input={: .2f}|pitch_accel={: .2f}".format(
-            np.clip(pitch_error, -3, 3), pitch_input, pitch_param[2] * pitch_accel
-        )
-    )
     motor.motor_speed(motor_fl=motor_fl, motor_fr=motor_fr, motor_rl=motor_rl, motor_rr=motor_rr)
